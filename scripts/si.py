4df0ec31e2cf44147fe1956f5c59ba3c0530ca23
Edward Linscott <edwardlinscott@gmail.com>
2022-10-21 13:12:27 +0200

    Adding slides for neutral excitations

--- scripts/si.py
+++ scripts/si.py
@@ -8,9 +8,7 @@ atoms = bulk('Si')
 
 # Define the projections for the Wannierization (same for filled and empty manifold)
 si_proj = [{'fsite': [0.25, 0.25, 0.25], 'ang_mtm': 'sp3'}]
-si_projs = ProjectionBlocks([{'filled': True, **si_proj},
-                             {'filled': False, **si_proj}],
-                             atoms=atoms)
+si_projs = ProjectionBlocks.from_list([si_proj, si_proj], atoms=atoms)
 
 # Create the workflow
 workflow = SinglepointWorkflow(atoms = atoms,
@@ -18,7 +16,7 @@ workflow = SinglepointWorkflow(atoms = atoms,
         ecutwfc = 40.0,
         kpoints = Kpoints(grid=[8, 8, 8], path='LGXKG', cell=atoms.cell),
         calculator_parameters = {'pw': {'nbnd': 10},
-        'w90_emp': {'dis_froz_max': 10.6, 'dis_win_max': 16.9}})
+        'w90': {'dis_froz_max': 10.6, 'dis_win_max': 16.9}})
 
 # Run the workflow
 workflow.run()
